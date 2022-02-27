09aa4aa0146a2bdc06f30d6b4c52040ee315813d
MJM <m.jungmuller@gmail.com>
2022-02-27 13:06:25 +0100

    activity

--- prototype/prototype.pml
+++ prototype/prototype.pml
@@ -3,9 +3,14 @@
     <Manifest src="manifest.xml" />
     <BehaviorDescriptions>
         <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Activity" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Activity advanced" xar="behavior.xar" />
     </BehaviorDescriptions>
     <Dialogs />
-    <Resources />
+    <Resources>
+        <File name="choice_sentences" src="Activity advanced/Aldebaran/choice_sentences.xml" />
+        <File name="choice_sentences_light" src="Activity advanced/Aldebaran/choice_sentences_light.xml" />
+    </Resources>
     <Topics />
     <IgnoredPaths />
     <Translations auto-fill="en_US">
--- prototype/translations/translation_en_US.ts
+++ prototype/translations/translation_en_US.ts
@@ -2,6 +2,60 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>Activity advanced/behavior.xar:/Failure speech</name>
+        <message>
+            <location filename="Activity advanced/behavior.xar" line="0"/>
+            <source>Don't worry, that happens to everyone!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Don't worry, that happens to everyone!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Activity advanced/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="Activity advanced/behavior.xar" line="0"/>
+            <source>Would you like to start over or redo last step?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Would you like to start over or redo last step?</translation>
+        </message>
+    </context>
+    <context>
+        <name>Activity advanced/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>Activity advanced/behavior.xar:/Say end</name>
+        <message>
+            <location filename="Activity advanced/behavior.xar" line="0"/>
+            <source>Great, you finished the activity!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Great, you finished the activity!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Activity/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="Activity/behavior.xar" line="0"/>
+            <source>Great, you finished the activity!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Great, you finished the activity!</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hello</source>
